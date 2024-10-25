3c42332b24ab58cd82c820cd1b418efa9607be57
masa <tailtikirinb1cs@gmail.com>
2024-10-25 10:03:56 +0900

    first commit

---
+++ src/main/java/raisetech/student/management2/Application.java
@@ -0,0 +1,28 @@
+package raisetech.student.management2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootApplication
+@RestController
+public class Application {
+	private  String name = "yamada";
+	private int age = 20;
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(Application.class, args);
+	}
+	@GetMapping("/name")
+	public String getName() {
+		return name;
+	}
+	@PostMapping("/name")
+	public  void setName(String name) {
+		this.name = name;
+	}
+}
