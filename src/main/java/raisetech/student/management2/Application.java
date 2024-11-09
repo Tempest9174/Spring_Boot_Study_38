21ba578aaf4b04acaaf52549dafee1d43ba93152
masa <tailtikirinb1cs@gmail.com>
2024-11-09 12:52:31 +0900

    レッスン23途中

--- src/main/java/raisetech/student/management2/Application.java
+++ src/main/java/raisetech/student/management2/Application.java
@@ -1,15 +1,10 @@
 package raisetech.student.management2;
 
-import ch.qos.logback.core.util.StringUtil;
-import org.apache.commons.lang3.StringUtils;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -31,29 +26,14 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
-	@GetMapping("/student")
-	public String getStudent(@RequestParam String name) {
-		Student student = repository.searchByName(name);
+	@GetMapping("/studentList")
+	public List<Student> getStudentList() {
+		return repository.search();
 		//変数でなくStudent studentなのか？
 		//表示
-			return student.getName() + " " + student.getAge() + "歳";
+		//return student.getName() + " " + student.getAge() + "歳";
 	}
 	//Student student = new Student();
 	//return name + " " + age + "歳";
 
-	@PostMapping("/student")
-	public void registerStudent(String name, int age) {
-		repository.registerStudent(name, age);
-		//挿入
-	}
-
-	@PatchMapping("/student")
-	public void updateStudentName(String name, int age) {
-		repository.updateStudent(name,age);
-
-	}
-	@DeleteMapping("/student")
-	public void deleteStudent(String name) {
-		repository.deleteStudent(name);
-	}
 }
---
+++ src/main/java/raisetech/student/management2/Cource.java
@@ -0,0 +1,5 @@
+package raisetech.student.management2;
+
+public class Cource {
+
+}
